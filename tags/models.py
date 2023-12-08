b5593d7bf49b8ca355f16bb7a18a9fbed80d7910
Pinto Tetteh Aaron <aaronpinto111@gmail.com>
2023-12-08 11:53:21 +0000

    custom manager for TaggedItem

--- tags/models.py
+++ tags/models.py
@@ -3,6 +3,16 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
 
+class TaggedItemManager(models.Manager):
+    def get_tags(self,object_type,object_id):
+        contentype = ContentType.objects.get_for_model(object_type)
+        
+        return TaggedItem.objects.select_related('tag').filter(
+            content_type=contentype,
+            object_id=object_id
+        )
+
+
 class Tag(models.Model):
     label = models.CharField(max_length=100, null=False)
 
@@ -11,6 +21,8 @@ class Tag(models.Model):
 
 
 class TaggedItem(models.Model):
+    objects = TaggedItemManager()
+    
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
