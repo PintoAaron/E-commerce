181a360bd5845409b48e793db912283e850ab8c5
Pinto Tetteh Aaron <aaronpinto111@gmail.com>
2024-01-11 16:27:53 +0000

    added django corsheaders

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'djoser',
     'django_filters',
     'debug_toolbar',
@@ -53,6 +54,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -70,6 +72,8 @@ INTERNAL_IPS = [
     "127.0.0.1",
 
 ]
+
+CORS_ALLOWED_ORIGINS = []
 
 TEMPLATES = [
     {
