1e6ef32696526d1a651e8d6aa562dc8ff0446b85
Pinto Tetteh Aaron <aaronpinto111@gmail.com>
2024-01-12 09:48:03 +0000

    sending mails to new users and when order is created

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -177,3 +177,11 @@ DJOSER = {
         'current_user': 'core.serializers.UserSerializer',
     },
 }
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
--- mail/signals/handlers.py
+++ mail/signals/handlers.py
@@ -1,13 +1,48 @@
+from django.core.mail import EmailMessage,BadHeaderError
+from django.db.models.signals import post_save
+from django.conf import settings
 from shop.signals import order_created_signal
 from django.dispatch import receiver
 from shop.models import Customer
 
 
 @receiver(order_created_signal)
-def send_mail(sender,**kwargs):
+def send_order_received_mail(sender,**kwargs):
     if kwargs['order']:
         order = kwargs['order']
         customer = Customer.objects.select_related('user').get(pk = order.customer_id)
-        #print(f"Hello {customer.user.first_name}, we have received your order and its under going processing")
+        try:
+            message = EmailMessage(
+                'Order Received',
+                f'Hello {customer.user.first_name}, we have successfully received your order and it is undergoing processing.',
+                'aaronpinto111@gmail.com',
+                [customer.user.email]
+            )
+            message.attach_file('media/Pintoshop.png')
+            message.send()
+        except BadHeaderError:
+            pass
         
-        
+
+@receiver(post_save,sender=settings.AUTH_USER_MODEL)    
+def send_welcome_message_to_new_user(sender,**kwargs):
+    if kwargs['created']:
+        to_email = kwargs['instance']
+        try:
+            message = EmailMessage(
+                'Welcome To PintoShop',
+                '''
+                Hi, 
+                You have successfullly created an account with PintoShop
+                Go ahead and start shopping
+                ''',
+                'aaronpinto111@gmail.com',
+                [to_email]
+                
+            )
+            message.send()
+        except BadHeaderError:
+            pass
+    
+
+  
