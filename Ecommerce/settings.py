bca5133bc06d2b23cc679750f93115eb2762c9d8
Pinto Tetteh Aaron <aaronpinto111@gmail.com>
2024-02-12 17:37:59 +0000

    added deployed domain to allowed hosts

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["pintoshop.onrender.com"]
 
 
 # Application definition
