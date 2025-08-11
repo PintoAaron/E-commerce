93792c1cf1053b8708c23c4579ad6d5280b95e1c
quantum-pinto <apinto@quantumgroupgh.com>
2025-08-11 16:09:57 +0000

    Update Settings for automated tests

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -116,14 +116,28 @@ WSGI_APPLICATION = 'Ecommerce.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'HOST': os.getenv('DB_HOST'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'PORT': '5432',
+        'NAME': os.environ.get('DB_NAME', 'test_db'),
+        'USER': os.environ.get('DB_USER', ''),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', ''),
+        'PORT': os.environ.get('DB_PORT', '5432'),
+        'TEST': {
+            'NAME': 'test_' + os.environ.get('DB_NAME', 'test_db'),
+        }
     }
 }
 
