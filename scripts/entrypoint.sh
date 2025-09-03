7c2f11a6ba41ed22b7dcc76bce83d790af28bc5c
quantum-pinto <apinto@quantumgroupgh.com>
2025-09-03 12:04:35 +0000

    modify for celery deployment

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-python manage.py migrate --noinput
-
-python manage.py collectstatic --noinput
-
-python manage.py add_collections
-
-python manage.py runserver 0.0.0.0:8000
+celery -A Ecommerce worker --loglevel=info    --concurrency=4 --pool=redis -Q default
